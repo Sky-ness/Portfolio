eca6a43a392f8e22ec73da2e14c6c2c62a0f8641
Antonin Duvauchel <antonin.duvauchel@laposte.net>
2024-05-26 20:05:43 +0200

    [ rooting ] implements root into app.component.html and  app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
+import { ContactComponent } from './contact/contact.component'
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: 'contact',
+        component: ContactComponent,
+    },
+    {
+        path: '**',
+        component: PageNotFoundComponent,
+    },
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,40 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { FooterComponent } from './footer/footer.component'
+import { TopBarComponent } from './top-bar/top-bar.component'
+import { ContactComponent } from './contact/contact.component'
+import { MenubarModule } from 'primeng/menubar'
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
+import { ButtonModule } from 'primeng/button'
+import { ReactiveFormsModule } from '@angular/forms'
+import { InputTextareaModule } from 'primeng/inputtextarea'
+import { ProjectsComponent } from './projects/projects.component'
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
+import { CheckboxModule } from 'primeng/checkbox'
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [],
-  bootstrap: [AppComponent],
+    declarations: [
+        AppComponent,
+        FooterComponent,
+        TopBarComponent,
+        ContactComponent,
+        PageNotFoundComponent,
+        ProjectsComponent,
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        MenubarModule,
+        ButtonModule,
+        ReactiveFormsModule,
+        InputTextareaModule,
+        FontAwesomeModule,
+        CheckboxModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
 export class AppModule {}
