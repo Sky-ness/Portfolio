56de010db29b7fddb91c2ac62411928188846690
Antonin.duvauchel.etu <antonin.duvauchel.etu@univ-lille.fr>
2024-06-03 00:01:47 +0200

    [Content] add content into footer component

--- src/app/features/footer/footer.component.ts
+++ src/app/features/footer/footer.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core'
+import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { IconProp } from '@fortawesome/fontawesome-svg-core'
 
 @Component({
     selector: 'app-footer',
     templateUrl: './footer.component.html',
     styleUrl: './footer.component.css',
 })
-export class FooterComponent {}
+export class FooterComponent {
+  protected faPhone = faPhone
+  protected faEnvelope = faEnvelope
+  protected faLinkedin = faLinkedin as IconProp
+  protected faGithub = faGithub as IconProp
+}
