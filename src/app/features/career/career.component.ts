7b964f6f5280c71559ec391f500d763e6b76ccfd
Antonin Duvauchel <antonin.duvauchel@laposte.net>
2024-06-02 14:29:01 +0200

    [ rename ] add career component

---
+++ src/app/features/career/career.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'app-career',
+    templateUrl: './career.component.html',
+    styleUrl: './career.component.css',
+})
+export class CareerComponent {}
