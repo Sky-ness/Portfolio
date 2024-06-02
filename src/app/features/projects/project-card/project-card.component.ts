37c55dcd361703377c938a570bf972cea891712e
Antonin Duvauchel <antonin.duvauchel@laposte.net>
2024-06-02 16:53:04 +0200

    [ adding ] add project card component

---
+++ src/app/features/projects/project-card/project-card.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core'
+
+@Component({
+    selector: 'app-project-card',
+    templateUrl: './project-card.component.html',
+    styleUrl: './project-card.component.css',
+})
+export class ProjectCardComponent {
+    @Input() project!: any
+}
