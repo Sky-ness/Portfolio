99c640da11a3a6d5219d20a58f97d53e61f29f62
Antonin.duvauchel.etu <antonin.duvauchel.etu@univ-lille.fr>
2024-06-03 00:03:16 +0200

    [content] add content into home component

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { faAngular, faGit, faJava, faLinux, faNode } from '@fortawesome/free-brands-svg-icons'
+import { IconProp } from '@fortawesome/fontawesome-svg-core'
 
 @Component({
   selector: 'app-home',
@@ -7,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  protected readonly faAngular = faAngular as IconProp
+  protected readonly faJava = faJava as IconProp
+  protected readonly faNode = faNode as IconProp
+  protected readonly faGit = faGit as IconProp
+  protected readonly faLinux = faLinux as IconProp
 }
