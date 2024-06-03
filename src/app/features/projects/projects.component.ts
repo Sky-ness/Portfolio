2a06b7d30fcc3f492cf65786cce95c8950d876c9
Antonin.duvauchel.etu <antonin.duvauchel.etu@univ-lille.fr>
2024-06-04 00:05:44 +0200

    [content] set images link and manage width card

--- src/app/features/projects/projects.component.ts
+++ src/app/features/projects/projects.component.ts
@@ -19,7 +19,7 @@ export class ProjectsComponent implements OnInit {
                     ' Nous avons eu comme contrainte de pouvoir réimplémenter un nouvel objet très rapidement pour cela nous avons utilisé la généricité.' +
                     ' Nous avons également implémenter une fonctionnalité qui consiste a donné un taux de fiabilité de notre model (la Robustesse) .',
                 pictures:
-                    'https://primefaces.org/cdn/primeng/images/card-ng.jpg',
+                    'https://imgs.search.brave.com/eyivNRCbegcF4a96CHf0U2ORJ5Ssb4tJC6ehCUV2wyo/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZWVrc2Zvcmdl/ZWtzLm9yZy93cC1j/b250ZW50L3VwbG9h/ZHMvY2xhc3NpZmlj/YXRpb24tMS5wbmc',
                 link: 'https://primefaces.org/cdn/primeng/images/card-ng.jpg',
             },
             {
@@ -31,17 +31,17 @@ export class ProjectsComponent implements OnInit {
                     "Dans notre jeux, il y a une fonction rejouer et un tableau des scores ou l'on voit  les autres joueurs. " +
                     "Et on peut choisir la couleur de son avatar et le nom qui s'affiche.",
                 pictures:
-                    'https://primefaces.org/cdn/primeng/images/card-ng.jpg',
+                    'https://imgs.search.brave.com/_dPUMdY8haFqTSIPbvOHj46WWiDR19Z4kf6koEmZxBQ/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9maXZl/cnItcmVzLmNsb3Vk/aW5hcnkuY29tL2lt/YWdlcy90X21haW4x/LHFfYXV0byxmX2F1/dG8scV9hdXRvLGZf/YXV0by9naWdzLzEx/NDI0ODgyNy9vcmln/aW5hbC9lODcyNjE2/OTUwZGRjZTllNTcy/ZTc2OTY5ZTQyMTk4/YjA5NzI5OGExL3By/b2Zlc3Npb25hbGx5/LXRlYWNoLXlvdS1h/Z2FyaW8ucG5n',
                 link: 'https://primefaces.org/cdn/primeng/images/card-ng.jpg',
             },
             {
-                title: 'Agar.io like',
+                title: 'Projet Agile',
                 date: '09/2022',
                 text:
                     'En début de semestre de la 2ème année de BUT informatique nous avons développé un jeu rétro ' +
                     'avec la méthode agile en java en utilisant des sprint de 4 heures et en faisant des démonstrations a la fin de ceux ci  .',
                 pictures:
-                    'assets/images/agario.webp',
+                    'https://imgs.search.brave.com/AIiZ4nj5jD3dBRF7B8fLs-vRbm8YOHlIPUKuOL3laXE/rs:fit:860:0:0/g:ce/aHR0cDovL3d3dy5j/b21tZW50Z2Vlay5j/b20vd3AtY29udGVu/dC91cGxvYWRzLzIw/MjAvMDEtMjkvdGVy/bWluYWwtbWFjb3Mt/dGV0cmlzLmpwZw',
                 link: 'https://primefaces.org/cdn/primeng/images/card-ng.jpg',
             },
         ]
